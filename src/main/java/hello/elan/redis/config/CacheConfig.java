3ac02452ee4c7b8d85ea8f05df9c7e2c336fb423
ElanZh <elan_zh@q163.com>
2019-12-22 17:05:45 +0800

    配置内存数据库

---
+++ src/main/java/hello/elan/redis/config/CacheConfig.java
@@ -0,0 +1,13 @@
+package hello.elan.redis.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 张一然
+ * @date 2019/12/22 下午4:29
+ * @Package hello.elan.redis.config
+ * @Description
+ */
+@Configuration
+public class CacheConfig {
+}
